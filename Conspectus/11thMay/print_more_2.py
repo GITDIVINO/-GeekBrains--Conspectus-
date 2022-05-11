ada929d5e168adcdb4107c7ac5bbed61701a5263
ALEX <mmm@mmm.com>
2022-05-11 06:26:59 +0200

    Just save to go my bus

--- Conspectus/11thMay/print_more_2.py
+++ Conspectus/11thMay/print_more_2.py
@@ -2,7 +2,11 @@ name = 'Kokos'
 age = 50
 
 #Выводим без переноса строки через ';'
-print('-------->')
+'''print('-------->')
 print(name, end=';')
 print(age,end=';')
-print('end', end=';')
+print('end', end=';')'''
+
+#Ввод данных с помощью input()
+result = input('Введите число: ')
+print('Вы ввели: ', result)
