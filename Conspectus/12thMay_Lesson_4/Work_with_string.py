9c4655675cd51769ba3ec1eb38aadf1ec6292ad5
ALEX <mmm@mmm.com>
2022-05-12 21:45:26 +0200

    commit on 12th of May for Lesson 4

---
+++ Conspectus/12thMay_Lesson_4/Work_with_string.py
@@ -0,0 +1,10 @@
+#Узнать длину строки
+hero_name = 'Hulk'
+print(len(hero_name))
+
+#Найти слово/букву в строке
+hero_phrase = 'Hulk like fire, Tor like water'
+print(hero_phrase.find('fire'))
+
+#Возвести в верхний регистр
+print(hero_name.upper())
