fcae9d559a44f0c0c4d5a04e1f3fa1e357d4a726
ALEX <mmm@mmm.com>
2022-05-16 22:47:14 +0200

    Game fighters, 16th of May

--- Conspectus/Hm_w_f_trnng/Lesson_8_ex_3.py
+++ Conspectus/Hm_w_f_trnng/Lesson_8_ex_3.py
@@ -7,13 +7,50 @@ damage = 50. ### Поэкспериментируйте с значениями 
 параметр damage атакующего и отнять это количество от health атакуемого. Функция должна сама работать со словарями
 и изменять их значения.'''
 
+#Задаём сущности с помощью словаря
+player = {'name': input('Please enter first_player name: '), 'health': 1000, 'damage': 50}
+enemy = {'name': input('Please enter second_player name: '), 'health': 1000, 'damage': 50}
+
+#Выводим на экран текущие параметры каждого игрока
+print(player['name'] + ' has ' + str(player['health']) + ' points of health and ' + str(player['damage']) + ' points of damage')
+print(enemy['name'] + ' has ' + str(enemy['health']) + ' points of health and ' + str(enemy['damage']) + ' points of damage')
+
+hit = None #Переменная, которая описывает куда будет нанесён удар
+
+#Описываем функцию для атаки
+def attack(unit_1,unit_2,hit):
+    hit = input('Куда будет наннесён удар? ')
+
+    if hit == 'head':
+        unit_2['health'] -= 5 * unit_1['damage']
+    elif hit == 'leg':
+        unit_2['health'] -= 3 * unit_1['damage']
+    elif hit == 'arm':
+        unit_2['health'] -= 2 * unit_1['damage']
 
 
+    if unit_2['health'] <= 0:
+        print(unit_2['name'] + ' is DEAD')
+        return False
+    else:
+        print(player['name'] + ' has ' + str(player['health']) + ' points of health')
+        print(enemy['name'] + ' has ' + str(enemy['health']) + ' points of health')
+        print('Fight continue........\n' + '_'*100 + '\n')
 
+round = 0
 
+while True:
+    round += 1
+    print(f'Round {round}')
+    first_fighter = input('Кто наносит удар? ')
+    if first_fighter == player['name']:
+        unit_1 = player
+        unit_2 = enemy
+    if first_fighter == enemy['name']:
+        unit_1 = enemy
+        unit_2 = player
 
-
-
+    attack(unit_1,unit_2,hit)
 
 
 
